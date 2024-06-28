40bb718d72a75dcab00247e3d20bd8c397e088e3
mateeusferro <mateus.03fernandesaraujo@gmail.com>
2024-06-27 21:46:59 -0300

    feat: adding the enable rabbit annotation

--- rabbitmq-w-spring-sender/src/main/java/com/ferro/mateus/rabbitmq_w_spring_sender/RabbitmqWSpringSenderApplication.java
+++ rabbitmq-w-spring-sender/src/main/java/com/ferro/mateus/rabbitmq_w_spring_sender/RabbitmqWSpringSenderApplication.java
@@ -1,8 +1,10 @@
 package com.ferro.mateus.rabbitmq_w_spring_sender;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableRabbit
 @SpringBootApplication
 public class RabbitmqWSpringSenderApplication {
 
