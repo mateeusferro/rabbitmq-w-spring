6cafbd7acbb2aff3856d99cda5c7f48ae8daa388
mateeusferro <mateus.03fernandesaraujo@gmail.com>
2024-06-27 21:48:28 -0300

    feat: configuring RabbitMq on sender

---
+++ rabbitmq-w-spring-sender/src/main/java/com/ferro/mateus/rabbitmq_w_spring_sender/config/RabbitMQConfig.java
@@ -0,0 +1,18 @@
+package com.ferro.mateus.rabbitmq_w_spring_sender.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${spring.rabbitmq.queue}")
+    private String queueName;
+
+    @Bean
+    public Queue queue() {
+        return new Queue(queueName, false);
+    }
+}
