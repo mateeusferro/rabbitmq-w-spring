d325bc85a5d634714fcd7b122f0c40820d848a37
mateeusferro <mateus.03fernandesaraujo@gmail.com>
2024-06-29 13:52:58 -0300

    refactor: improving the logic to save the payment on db

--- rabbitmq-w-spring-receiver/src/main/java/com/ferro/mateus/rabbitmq_w_spring_receiver/service/impl/PaymentServiceImpl.java
+++ rabbitmq-w-spring-receiver/src/main/java/com/ferro/mateus/rabbitmq_w_spring_receiver/service/impl/PaymentServiceImpl.java
@@ -4,8 +4,13 @@ import com.ferro.mateus.rabbitmq_w_spring_receiver.domain.entity.Payment;
 import com.ferro.mateus.rabbitmq_w_spring_receiver.domain.repository.PaymentRepository;
 import com.ferro.mateus.rabbitmq_w_spring_receiver.listener.dto.PaymentEvent;
 import com.ferro.mateus.rabbitmq_w_spring_receiver.service.PaymentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @Service
 public class PaymentServiceImpl implements PaymentService {
@@ -16,11 +21,20 @@ public class PaymentServiceImpl implements PaymentService {
     @Override
     public void processPayment(PaymentEvent paymentEvent) {
         for (int i = 1; i <= paymentEvent.installmentQuantity(); i++) {
+            BigDecimal installmentQuantity = BigDecimal.valueOf(
+                    paymentEvent.installmentQuantity());
+            BigDecimal monthGrossTotal = paymentEvent.grossTotal().divide(
+                    installmentQuantity,2, RoundingMode.CEILING);
+            BigDecimal totalWithInterestRate =
+                    monthGrossTotal
+                            .multiply(BigDecimal.ONE
+                                    .add(paymentEvent.interestRate()));
+
             Payment payment = Payment.builder()
                     .paymentInstallmentIndex(i)
-                    .paymentGrossTotal(paymentEvent.grossTotal())
+                    .paymentGrossTotal(monthGrossTotal)
                     .paymentInterestRate(paymentEvent.interestRate())
-                    .paymentTotalWithInterest(paymentEvent.grossTotal())
+                    .paymentTotalWithInterest(totalWithInterestRate)
                     .paymentDueDate(paymentEvent.dueDate().plusMonths(i))
                     .paymentType(paymentEvent.paymentType())
                     .paymentFinancingId(paymentEvent.id())
